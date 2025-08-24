6bc5c5a3ee652d79a117b60caff12366dccf38a1
김정욱 <ian@gimjeong-ug-ui-Macmini.local>
2025-08-24 09:09:58 +0900

    env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { envKeys } from './common/const/env.const';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3305,
-      username: 'root',
-      password: 'root',
-      // database: 'test',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'prodduction').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
+        type: 'mysql',
+        host: config.get(envKeys.dbHost),
+        port: config.get(envKeys.dbPort),
+        username: config.get(envKeys.dbUsername),
+        password: config.get(envKeys.dbPassword),
+        database: config.get(envKeys.dbDatabase),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: config.get(envKeys.env) === 'dev',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
---
+++ src/common/const/env.const.ts
@@ -0,0 +1,15 @@
+const env = 'NODE_ENV';
+const dbHost = 'DB_HOST';
+const dbPort = 'DB_PORT';
+const dbUsername = 'DB_USERNAME';
+const dbPassword = 'DB_PASSWORD';
+const dbDatabase = 'DB_DATABASE';
+
+export const envKeys = {
+  env,
+  dbHost,
+  dbPort,
+  dbUsername,
+  dbPassword,
+  dbDatabase,
+};
